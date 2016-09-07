dccff82f4a22be84fee9380110674f5066a5534f
E1evenJ <xj363057422@126.com>
2016-09-07 11:37:40 +0800

    DDD

--- modules/mq/server/controllers/hello.server.controller.js
+++ modules/mq/server/controllers/hello.server.controller.js
@@ -5,7 +5,11 @@ const path = require('path');
 const client = require(path.resolve('./config/lib/stomp'));
 const logger = require(path.resolve('./config/lib/logger'));
 const mqName = require(path.resolve('./modules/mq/server/common/mq.server.common.name.js'));
-
+/**
+ * fdskfjsdlkjf
+ * @param req
+ * @param res
+ */
 exports.publish = function (req, res) {
   if (req.body) {
     client.publish(mqName.HELLO, req.body, function (err) {
