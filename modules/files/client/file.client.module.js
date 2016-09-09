281fdb3a6becaf22a42f76bf50503eb8975e1e4b
bournewang <jeffking838@gmail.com>
2016-09-09 13:21:27 +0800

    file client

--- modules/files/client/file.client.module.js
+++ modules/files/client/file.client.module.js
@@ -2,5 +2,5 @@
     'use strict';
 
     app.registerModule('files', ['core']);
-    app.registerModule('files.routes', ['ui.router', 'core.routes', 'files.services']);
+    app.registerModule('files.routes', ['ui.router', 'core.routes']);
 }(ApplicationConfiguration));
