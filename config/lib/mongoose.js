9a0559c362d7cf52e05ec0b3978f4c19bae77828
bournewang <jeffking838@gmail.com>
2016-09-19 14:41:44 +0800

    fix an error totalCount bug  ...

--- config/lib/mongoose.js
+++ config/lib/mongoose.js
@@ -8,8 +8,54 @@ const config = require('../config'),
     path = require('path'),
     mongoose = require('mongoose');
 const Pagination = require('./Pagination');
+const logger = require(path.resolve('./config/lib/logger'));
 
-let baseFieldPlugin = function (schema, options) {
+
+// Load the mongoose models
+module.exports.loadModels = loadModels;
+// Initialize Mongoose
+module.exports.connect = connect;
+
+module.exports.disconnect = disconnect;
+
+function connect(cb) {
+    var _this = this;
+
+    var db = mongoose.connect(config.db.uri, config.db.options, function (err) {
+        // Log Error
+        if (err) {
+            logger.error(chalk.red('Could not connect to MongoDB!'));
+            logger.error(err);
+        } else {
+
+            // Enabling mongoose debug mode if required
+            mongoose.set('debug', config.db.debug);
+
+            // Call callback FN
+            if (cb) cb(db);
+        }
+    });
+}
+
+function disconnect(cb) {
+    mongoose.disconnect(function (err) {
+        logger.info(chalk.yellow('Disconnected from MongoDB.'));
+        cb(err);
+    });
+}
+
+function loadModels(callback) {
+    mongoose.plugin(baseFieldPlugin);
+    mongoose.plugin(paginationPlugin);
+    // Globbing model files
+    config.files.server.models.forEach(function (modelPath) {
+        require(path.resolve(modelPath));
+    });
+
+    if (callback) callback();
+}
+
+function baseFieldPlugin(schema) {
     schema.set('toJSON', {virtuals: true});
     schema.add({
         updated: {
@@ -29,15 +75,15 @@ let baseFieldPlugin = function (schema, options) {
         this.updated = Date.now;
         next();
     });
-};
+}
 
-let paginationPlugin = function (schema, options) {
+function paginationPlugin(schema) {
 
     schema.statics.findPagination = function (query, sort, pageSize,
                                               pageNum) {
         let self = this;
         return new Promise(function (resolve, reject) {
-            self.count({}, function (err, totalCount) {
+            self.count(query, function (err, totalCount) {
                 if (err) {
                     throw err;
                 }
@@ -56,43 +102,4 @@ let paginationPlugin = function (schema, options) {
             });
         });
     };
-};
-
-// Load the mongoose models
-module.exports.loadModels = function (callback) {
-    mongoose.plugin(baseFieldPlugin);
-    mongoose.plugin(paginationPlugin);
-    // Globbing model files
-    config.files.server.models.forEach(function (modelPath) {
-        require(path.resolve(modelPath));
-    });
-
-    if (callback) callback();
-};
-
-// Initialize Mongoose
-module.exports.connect = function (cb) {
-    var _this = this;
-
-    var db = mongoose.connect(config.db.uri, config.db.options, function (err) {
-        // Log Error
-        if (err) {
-            console.error(chalk.red('Could not connect to MongoDB!'));
-            console.log(err);
-        } else {
-
-            // Enabling mongoose debug mode if required
-            mongoose.set('debug', config.db.debug);
-
-            // Call callback FN
-            if (cb) cb(db);
-        }
-    });
-};
-
-module.exports.disconnect = function (cb) {
-    mongoose.disconnect(function (err) {
-        console.info(chalk.yellow('Disconnected from MongoDB.'));
-        cb(err);
-    });
-};
+}
