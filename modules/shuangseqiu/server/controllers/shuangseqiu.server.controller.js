9eb0324c44646a9691fec17b8d435ba7937b26ed
bournewang <jeffking838@gmail.com>
2016-09-22 15:53:41 +0800

    Shuange se qiu update delete

--- modules/shuangseqiu/server/controllers/shuangseqiu.server.controller.js
+++ modules/shuangseqiu/server/controllers/shuangseqiu.server.controller.js
@@ -8,9 +8,20 @@ const mongoose = require('mongoose');
 const Shuangseqiu = mongoose.model('Shuangseqiu');
 const CommonError = require(path.resolve('./config/error/CommonError'));
 const PaginationUtil = require(path.resolve('./modules/core/server/common/PaginationUtil'));
+const logger = require(path.resolve('./config/lib/logger'));
 
-exports.create = function (req, res) {
+exports.create = create;
 
+/**
+ * List of schedules
+ */
+exports.list = list;
+
+exports.update = update;
+
+exports.remove = remove;
+
+function create(req, res) {
     let shuangseqiu = new Shuangseqiu(req.body);
     shuangseqiu.save(function (err) {
         if (err) {
@@ -20,12 +31,9 @@ exports.create = function (req, res) {
         }
     });
 
-};
+}
 
-/**
- * List of schedules
- */
-exports.list = function (req, res) {
+function list(req, res) {
     let size = parseInt(req.params.pageSize, 0);
     let pageSize = size > 0 ? size : 5;
     let pageNum = parseInt(req.params.pageNum, 0);
@@ -41,4 +49,47 @@ exports.list = function (req, res) {
             logger.error(err);
             throw new CommonError('系统错误');
         });
-};
+}
+
+function update(req, res) {
+    Shuangseqiu.findById(req.params.id, function (err, obj) {
+        if (err) {
+            throw err;
+        }
+        if (obj) {
+            obj.update(req.body, function (err) {
+                if (err) {
+                    throw err;
+                } else {
+                    Shuangseqiu.findById(req.params.id, function (err, obj) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.json(obj);
+                    });
+                }
+            });
+        } else {
+            res.end();
+        }
+    });
+}
+
+function remove(req, res) {
+    Shuangseqiu.findById(req.params.id, function (err, obj) {
+        if (err) {
+            throw err;
+        }
+        if (obj) {
+            obj.remove(req.body, function (err, dObj) {
+                if (err) {
+                    throw err;
+                } else {
+                    res.json(dObj);
+                }
+            });
+        } else {
+            res.end();
+        }
+    });
+}
--- modules/shuangseqiu/server/routes/shuangseqiu.server.route.js
+++ modules/shuangseqiu/server/routes/shuangseqiu.server.route.js
@@ -6,4 +6,5 @@ var shuangseqius = require('../controllers/shuangseqiu.server.controller.js');
 module.exports = function (app) {
     app.route('/api/shuangseqiu/:pageSize/:pageNum').get(shuangseqius.list);
     app.route('/api/shuangseqiu').post(shuangseqius.create);
+    app.route('/api/shuangseqiu/:id').put(shuangseqius.update).delete(shuangseqius.remove);
 };
