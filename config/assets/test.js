9eb0f9c802dccbf0ff65b7d5968b17497295775b
bournewang <jeffking838@gmail.com>
2016-09-22 11:31:39 +0800

    as you can see

--- config/assets/test.js
+++ config/assets/test.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  tests: {
-    client: ['modules/*/tests/client/**/*.js'],
-    //  server: ['modules/*/tests/server/**/*.js'],
-    server: ['modules/schedules/tests/server/common/ScheduleHelper.test.js'],
-    e2e: ['modules/*/tests/e2e/**/*.js']
-  }
+    tests: {
+        client: ['modules/*/tests/client/**/*.js'],
+        server: ['modules/*/tests/server/**/*.js'],
+        // server: ['modules/schedules/tests/server/common/ScheduleHelper.test.js'],
+        e2e: ['modules/*/tests/e2e/**/*.js']
+    }
 };
